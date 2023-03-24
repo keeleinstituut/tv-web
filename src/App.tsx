7b6af4b3f2357e1eff0e146aa8d9c39875171e19
Kaarel Kastehein <kaarel@singleton.ee>
2023-03-24 16:11:05 +0200

    New - 66 - Add basic keycloak demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,37 @@
-import { FC } from 'react'
+import { FC, useEffect, useState } from 'react'
 import MainLayout from 'components/organisms/MainLayout/MainLayout'
+import Keycloak from 'keycloak-js'
 
-const App: FC = () => (
-  <MainLayout>
-    <div />
-  </MainLayout>
-)
+const keycloak = new Keycloak()
+
+const App: FC = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false)
+  const [userId, setUserId] = useState<string | undefined>('')
+
+  useEffect(() => {
+    const checkIfUserLoggedIn = async () => {
+      const isUserLoggedIn = await keycloak.init({})
+      if (isUserLoggedIn) {
+        setIsAuthenticated(true)
+        setUserId(keycloak?.idTokenParsed?.aud)
+      } else {
+        setIsAuthenticated(false)
+      }
+    }
+    checkIfUserLoggedIn()
+  }, [])
+
+  const testLogin = () => keycloak && keycloak.login()
+  return (
+    <MainLayout>
+      <div />
+      {userId && isAuthenticated ? (
+        <h1>{userId}</h1>
+      ) : (
+        <button onClick={testLogin}>Test Login</button>
+      )}
+    </MainLayout>
+  )
+}
 
 export default App
