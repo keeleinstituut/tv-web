763d5c263dc6a17c2e80b1c839873d6a0010df94
Kaarel Kastehein <kaarel@singleton.ee>
2023-03-24 13:04:39 +0200

    New - 99 - Set up base boilerplate with some libraries. WIP - library for handling navigation not added yet

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { FC } from 'react'
+import MainLayout from 'components/organisms/MainLayout/MainLayout'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: FC = () => (
+  <MainLayout>
+    <div />
+  </MainLayout>
+)
 
-export default App;
+export default App
