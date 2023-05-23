56994fead880617bf8c1394d3dd7641e7ce3f572
Kaarel Kastehein <kaarel@singleton.ee>
2023-05-23 22:53:57 +0300

    New - 106 - Added istitution selection modal + RootModal for easier access to showing modals

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,6 @@
 import ApiClient from './ApiClient'
 
-export const apiClient = new ApiClient(process.env.REACT_APP_API_URL || '')
+export const apiClient = new ApiClient('')
 export const authApiClient = new ApiClient(
   process.env.REACT_APP_KEYCLOAK_url || ''
 )
--- src/components/templates/AuthWrapper/AuthWrapper.tsx
+++ src/components/templates/AuthWrapper/AuthWrapper.tsx
@@ -1,6 +1,7 @@
 import { FC, PropsWithChildren } from 'react'
 import { Outlet } from 'react-router-dom'
 import useKeycloak, { AuthContext } from 'hooks/useKeycloak'
+import ModalRoot from 'components/organisms/modals'
 import Landing from 'pages/Landing/Landing'
 
 const AuthWrapper: FC<PropsWithChildren> = () => {
@@ -17,6 +18,7 @@ const AuthWrapper: FC<PropsWithChildren> = () => {
       }}
     >
       {isUserLoggedIn ? <Outlet /> : <Landing />}
+      <ModalRoot />
     </AuthContext.Provider>
   )
 }
