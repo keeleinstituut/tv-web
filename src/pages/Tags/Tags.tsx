b673c902f94eb5c93fd287539709c48f4206020a
Inga Sokman <inga@singleton.ee>
2023-07-18 16:06:59 +0300

    Wip-93 Change input styles, add DynamicForm for text inputFields

--- src/pages/Tags/Tags.tsx
+++ src/pages/Tags/Tags.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, FC, useState } from 'react'
+import { FC, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import Tooltip from 'components/organisms/Tooltip/Tooltip'
 import TagsCheatSheet from 'components/molecules/cheatSheets/TagManagementCheatSheet'
@@ -8,52 +8,48 @@ import DynamicForm, {
   InputTypes,
 } from 'components/organisms/DynamicForm/DynamicForm'
 import { ReactComponent as Add } from 'assets/icons/add.svg'
-import { useForm } from 'react-hook-form'
+import { useForm, useWatch } from 'react-hook-form'
 import Button, {
   AppearanceTypes,
   IconPositioningTypes,
 } from 'components/molecules/Button/Button'
 
 import classes from './classes.module.scss'
-import { map } from 'lodash'
 // import { useFetchTags } from 'hooks/requests/useTags'
 
+interface ObjectType {
+  [key: string]: string
+}
+
 type FormValues = {
-  tagInput?: string
+  // tagInput?: string
+  tagInput?: ObjectType
   tagCategorySelection?: string
 }
 
 const Tags: FC = () => {
   const { t } = useTranslation()
-  const {
-    control,
-    // handleSubmit,
-    // formState: { isSubmitting, isDirty, isValid },
-    // setError,
-  } = useForm<FormValues>({
+  const { control, handleSubmit } = useForm<FormValues>({
     reValidateMode: 'onSubmit',
-    // defaultValues: defaultValues,
   })
 
-  const testFields: FieldProps<FormValues>[] = [
+  const tagFields: FieldProps<FormValues>[] = [
     {
       inputType: InputTypes.Text,
       ariaLabel: t('tag.tag_name'),
       label: 'Nimetus',
-      name: 'tagInput',
+      // name: 'tagInput',
+      name: `tagInput.${1}`,
       placeholder: t('tag.tag_input'),
       type: 'text',
       rules: {
         required: true,
-        // validate: emailValidator,
       },
       className: classes.tagInputField,
     },
   ]
 
-  console.log('testFields', testFields)
-
-  const testFields2: FieldProps<FormValues>[] = [
+  const categoryFields: FieldProps<FormValues>[] = [
     {
       inputType: InputTypes.Selections,
       name: 'tagCategorySelection',
@@ -73,32 +69,43 @@ const Tags: FC = () => {
     },
   ]
 
-  const initialInput = { type: 'text', id: 1, value: '' }
-  const [inputFields, setInputFields] = useState([initialInput])
+  const [inputFields, setInputFields] = useState(tagFields)
+
+  console.log('inputFields state', inputFields)
 
   const addInputField = () => {
     setInputFields([
       ...inputFields,
       {
+        inputType: InputTypes.Text,
+        ariaLabel: t('tag.tag_name'),
+        label: 'Nimetus',
+        name: `tagInput.${inputFields?.length + 1}`,
+        placeholder: t('tag.tag_input'),
         type: 'text',
-        id: inputFields?.length + 1,
-        value: '',
+        rules: {
+          required: true,
+        },
+        className: classes.tagInputField,
       },
     ])
   }
 
-  const handleInputChange = (
-    event: ChangeEvent<HTMLInputElement>,
-    id: number
-  ) => {
-    const updatedInputFields = map(inputFields, (field) => {
-      if (field?.id === id) {
-        return { ...field, value: event.target.value }
-      }
-      return field
-    })
-    setInputFields(updatedInputFields)
-  }
+  // const handleInputChange = (
+  //   event: ChangeEvent<HTMLInputElement>,
+  //   id: number
+  // ) => {
+  //   const updatedInputFields = map(inputFields, (field) => {
+  //     if (field?.id === id) {
+  //       return { ...field, value: event.target.value }
+  //     }
+  //     return field
+  //   })
+  //   setInputFields(updatedInputFields)
+  // }
+
+  const formValue = useWatch({ control })
+  console.log('formValue', formValue)
 
   return (
     <>
@@ -115,21 +122,10 @@ const Tags: FC = () => {
           <p>{t('tag.naming_tag')}</p>
         </div>
 
-        <div className={classes.tagSeparator} />
+        <div className={classes.addingTagsSeparator} />
 
         <div className={classes.gridItemTwo}>
-          {map(inputFields, (field) => {
-            console.log('inputFields', inputFields)
-            return (
-              <input
-                key={field.id}
-                type={field.type}
-                value={field.value}
-                onChange={(event) => handleInputChange(event, field.id)}
-                className={classes.tagInputField}
-              />
-            )
-          })}
+          <DynamicForm fields={inputFields} control={control} />
           <Button
             appearance={AppearanceTypes.Text}
             iconPositioning={IconPositioningTypes.Left}
@@ -141,7 +137,8 @@ const Tags: FC = () => {
         </div>
 
         <div className={classes.gridItemThree}>
-          <DynamicForm fields={testFields2} control={control} />
+          <DynamicForm fields={categoryFields} control={control} />
+
           <Button
             children={t('button.add')}
             type="submit"
