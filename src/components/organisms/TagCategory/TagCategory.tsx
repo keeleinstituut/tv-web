1b5243e43b450015394d10a68e3bcc60c54210ba
Inga Sokman <inga@singleton.ee>
2023-07-26 18:33:30 +0300

    Wip-125 Fix ts errors, remove redundant code, add tag fileds to modal

--- src/components/organisms/TagCategory/TagCategory.tsx
+++ src/components/organisms/TagCategory/TagCategory.tsx
@@ -1,7 +1,7 @@
 import Container from 'components/atoms/Container/Container'
 import Button, { AppearanceTypes } from 'components/molecules/Button/Button'
-import { filter, flatMap, groupBy, includes, map } from 'lodash'
-import { FC, useMemo } from 'react'
+import { filter, groupBy, includes, map } from 'lodash'
+import { FC } from 'react'
 import { useTranslation } from 'react-i18next'
 import { ReactComponent as EditIcon } from 'assets/icons/edit.svg'
 import { ModalTypes, showModal } from '../modals/ModalRoot'
@@ -16,140 +16,74 @@ import classes from './classes.module.scss'
 
 type TagCategoryTypes = {
   tags?: TagType[]
-  //   control: Control<FormValues>
 }
-
-// export type FormValues = {
-//   type?: number
-//   tags?: [
-//     {
-//       id?: string
-//       name?: string
-//     }
-//   ]
-// }
 
 type EditTagType = {
   id?: string
   name?: string
 }
 
-// export type FormValues = {
-//   tags?: EditTagType[]
-// }
-
-type FormValues = {
-  tags: {
-    [key: string]: EditTagType[]
-  }
+export type FormValues = {
+  tags?: EditTagType[]
 }
-
-const TagCategory: FC<TagCategoryTypes> = ({
-  tags,
-  //  control
-}) => {
+const TagCategory: FC<TagCategoryTypes> = ({ tags }) => {
   const { t } = useTranslation()
   const { userPrivileges } = useAuth()
 
   const groupedCategoryData = groupBy(tags, 'type')
   const uniqueCategoryTypes = Object.keys(groupedCategoryData)
 
-  const defaultValues = map(uniqueCategoryTypes, (type) => {
-    return {
-      type: type,
-      tags: map(groupedCategoryData[type], ({ id, name }) => ({ id, name })),
-    }
-  })
-
-  console.log('defaultValues', defaultValues)
-
-  const defaultValues2 = map(uniqueCategoryTypes, (type) => {
-    return {
-      type: type,
-      tags: map(groupedCategoryData[type], ({ id, name }) => ({ id, name })),
-    }
-  })
-
-  // const defaultValues4 = map(uniqueCategoryTypes, (type) => {
+  // const defaultValues = map(uniqueCategoryTypes, (type) => {
   //   return {
+  //     // type: type,
   //     tags: map(groupedCategoryData[type], ({ id, name }) => ({ id, name })),
   //   }
   // })
 
-  const defaultValues3 = {
-    type: undefined,
-    tags: [],
-  }
-
-  // const defaultValues4 = useMemo(
-  //   () =>
-  //     map(uniqueCategoryTypes, (type) => {
-  //       return {
-  //         type: type,
-  //         tags: map(groupedCategoryData[type], ({ id, name }) => ({
-  //           id,
-  //           name,
-  //         })),
-  //       }
-  //     }),
-  //   [groupedCategoryData, uniqueCategoryTypes]
-  // )
-
-  // const defaultValues8 = useMemo(() => {
-  //   return {
-  //     tags: tags2.map(({ id, name }) => ({
-  //       id: id,
-  //       name: name,
-  //     })),
-  //   }
-  // }, [tags2])
+  console.log('tags', tags)
 
   const {
     control,
-    handleSubmit,
-    formState: { dirtyFields },
+    // handleSubmit,
+    // formState: { dirtyFields },
   } = useForm<FormValues>({
     reValidateMode: 'onSubmit',
     // values: defaultValues8,
   })
 
-  // const editTagFields: FieldProps<FormValues>[] = flatMap(
-  //   defaultValues,
-  //   (tag) =>
-  //     map(tag.tags, ({ id, name }) => ({
-  //       inputType: InputTypes.Text,
-  //       ariaLabel: name,
-  //       label: tag.type,
-  //       name: `tagInput.${id}`,
-  //     }))
-  // )
+  const defaultValues4 = map(uniqueCategoryTypes, (type) => {
+    return {
+      tags: map(groupedCategoryData[type], ({ id, name }) => ({ id, name })),
+    }
+  })
 
-  // const editTagFields: FieldProps<FormValues>[] = flatMap(
-  //   defaultValues,
-  //   (tag) =>
-  //     map(tag.tags, ({ id, name }) => ({
-  //       inputType: InputTypes.Text,
-  //       ariaLabel: name,
-  //       label: tag.type,
-  //       name: 'tags',
-  //     }))
-  // )
+  const editTagFields: FieldProps<FormValues>[] = map(tags, (tag, index) => {
+    console.log('tag', tag)
+    return {
+      inputType: InputTypes.Text,
+      ariaLabel: tag.name ?? '',
+      label: tag.type ?? '',
+      name: `tags.${index}.name`,
+    }
+  })
 
-  // const handleCategoryEdit = (type: string) => {
-  //   const filteredArrayByType = filter(editTagFields, { label: type })
+  const handleCategoryEdit = (type: string) => {
+    const filteredArrayByType = filter(editTagFields, { label: type })
 
-  //   console.log('type editmodal', type)
-  //   console.log('editTagFields modal', editTagFields)
-  //   showModal(ModalTypes.UserAndRoleManagement, {
-  //     title: t('modal.edit_category_tag'),
-  //     size: ModalSizeTypes.Narrow,
-  //     cancelButtonContent: t('button.cancel'),
-  //     proceedButtonContent: t('button.save'),
-  //     dynamicForm: (
-  //       <DynamicForm fields={filteredArrayByType} control={control} />
-  //     ),
-  //   })
-  // }
+    console.log('type editmodal', type)
+    console.log('editTagFields modal', editTagFields)
+    showModal(ModalTypes.UserAndRoleManagement, {
+      title: t('modal.edit_category_tag'),
+      size: ModalSizeTypes.Narrow,
+      cancelButtonContent: t('button.cancel'),
+      proceedButtonContent: t('button.save'),
+      dynamicForm: (
+        <DynamicForm fields={filteredArrayByType} control={control} />
+      ),
+    })
+  }
+
+  console.log('defaultValues4', defaultValues4)
 
   console.log('groupedCategoryData', groupedCategoryData)
   console.log('uniqueCategoryTypes', uniqueCategoryTypes)
@@ -166,7 +100,7 @@ const TagCategory: FC<TagCategoryTypes> = ({
                 appearance={AppearanceTypes.Text}
                 icon={EditIcon}
                 className={classes.editIcon}
-                // onClick={() => handleCategoryEdit(type)}
+                onClick={() => handleCategoryEdit(type)}
                 hidden={!includes(userPrivileges, Privileges.EditTag)}
               >
                 <span className={classes.tagName}>{t('button.change')}</span>
