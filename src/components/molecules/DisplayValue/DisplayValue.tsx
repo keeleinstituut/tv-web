cc3cdc2e47e3baae703eeab62562538d82f47fc8
Kaarel Kastehein <kaarel@singleton.ee>
2023-08-03 19:45:26 +0300

    Improve - 130-extra-inputs - Add Date-time picker + improve DisplayValue

--- src/components/molecules/DisplayValue/DisplayValue.tsx
+++ src/components/molecules/DisplayValue/DisplayValue.tsx
@@ -1,16 +1,44 @@
-import { FC } from 'react'
-import { join, isArray } from 'lodash'
+import { FC, useMemo } from 'react'
+import { join, isArray, reduce } from 'lodash'
 import classes from './classes.module.scss'
 
 export interface DisplayValueProps {
-  value?: string | number | boolean | readonly string[]
+  value?:
+    | string
+    | number
+    | boolean
+    | readonly string[]
+    | { [key: string]: string }
+  label?: string | JSX.Element
 }
 
-const DisplayValue: FC<DisplayValueProps> = ({ value }) => {
+const DisplayValue: FC<DisplayValueProps> = ({ value, label }) => {
+  const displayedValue = useMemo(() => {
+    if (isArray(value)) return join(value, ', ')
+    if (
+      typeof value === 'string' ||
+      typeof value === 'number' ||
+      typeof value === 'boolean'
+    ) {
+      return value
+    }
+    if (typeof value === 'boolean')
+      return reduce(
+        value,
+        (result, valueString) => {
+          if (!valueString) return result
+          return `${result} ${valueString}`
+        },
+        ''
+      )
+  }, [value])
   return (
-    <span className={classes.cellValue}>
-      {isArray(value) ? join(value, ', ') : value}
-    </span>
+    <>
+      <span hidden={!label} className={classes.label}>
+        {label}
+      </span>
+      <span className={classes.cellValue}>{displayedValue}</span>
+    </>
   )
 }
 
