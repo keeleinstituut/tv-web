2c2882b4d75b8bad6c987bacaae5c6ed0459b428
Inga Sokman <inga@singleton.ee>
2023-10-10 14:01:39 +0300

    New-429 Add serach for vendors and users list

--- src/pages/VendorsDatabase/VendorsDatabase.tsx
+++ src/pages/VendorsDatabase/VendorsDatabase.tsx
@@ -1,6 +1,6 @@
-import { FC, useCallback } from 'react'
+import { FC, useCallback, useState } from 'react'
 import { useTranslation } from 'react-i18next'
-import { isEmpty, includes } from 'lodash'
+import { isEmpty, includes, debounce } from 'lodash'
 import { Root } from '@radix-ui/react-form'
 import Loader from 'components/atoms/Loader/Loader'
 import useAuth from 'hooks/useAuth'
@@ -11,6 +11,7 @@ import Button, { AppearanceTypes } from 'components/molecules/Button/Button'
 import classes from './classes.module.scss'
 import Tooltip from 'components/organisms/Tooltip/Tooltip'
 import { showModal, ModalTypes } from 'components/organisms/modals/ModalRoot'
+import TextInput from 'components/molecules/TextInput/TextInput'
 
 const VendorsDatabase: FC = () => {
   const { t } = useTranslation()
@@ -28,6 +29,16 @@ const VendorsDatabase: FC = () => {
   const handleOpenVendorsEditModal = useCallback(() => {
     showModal(ModalTypes.VendorsEdit, {})
   }, [])
+
+  const [searchValue, setSearchValue] = useState<string>('')
+
+  const handleSearchVendors = useCallback(
+    (event: { target: { value: string } }) => {
+      setSearchValue(event.target.value)
+      debounce(handleFilterChange, 300)({ fullname: event.target.value })
+    },
+    [handleFilterChange]
+  )
 
   return (
     <>
@@ -56,6 +67,16 @@ const VendorsDatabase: FC = () => {
       </div>
       <Root>
         <Loader loading={isLoading && isEmpty(vendors)} />
+        <TextInput
+          name={'search'}
+          ariaLabel={t('placeholder.search_by_name')}
+          placeholder={t('placeholder.search_by_name')}
+          value={searchValue}
+          onChange={handleSearchVendors}
+          className={classes.searchInput}
+          inputContainerClassName={classes.generalVendorsListInput}
+          isSearch
+        />
         <VendorsTable
           data={vendors}
           hidden={isEmpty(vendors)}
