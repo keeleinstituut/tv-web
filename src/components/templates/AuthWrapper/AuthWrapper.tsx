56978ef23c14f919470ec2fec66b2492a75c34e5
Kaarel Kastehein <kaarel@singleton.ee>
2023-12-12 09:26:46 +0200

    WIP - 609 - test clearing personalized info after logout

--- src/components/templates/AuthWrapper/AuthWrapper.tsx
+++ src/components/templates/AuthWrapper/AuthWrapper.tsx
@@ -1,4 +1,4 @@
-import { FC, PropsWithChildren } from 'react'
+import { FC, PropsWithChildren, useCallback } from 'react'
 import { Outlet } from 'react-router-dom'
 import useKeycloak, { AuthContext } from 'hooks/useKeycloak'
 import ModalRoot from 'components/organisms/modals/ModalRoot'
@@ -6,10 +6,20 @@ import Landing from 'pages/Landing/Landing'
 import useAuthRedirect from 'hooks/useAuthRedirect'
 import Loader from 'components/atoms/Loader/Loader'
 import classes from './classes.module.scss'
+import { useQueryClient } from '@tanstack/react-query'
 
 const AuthWrapper: FC<PropsWithChildren> = () => {
   const { keycloak, isUserLoggedIn, userInfo, isLoading } = useKeycloak()
+  const queryClient = useQueryClient()
   useAuthRedirect(userInfo?.tolkevarav?.privileges)
+
+  const handleLogout = useCallback(() => {
+    if (keycloak) {
+      keycloak.logout()
+      queryClient.clear()
+      keycloak.clearToken()
+    }
+  }, [keycloak, queryClient])
 
   return (
     <AuthContext.Provider
@@ -17,7 +27,7 @@ const AuthWrapper: FC<PropsWithChildren> = () => {
         isUserLoggedIn,
         userInfo,
         login: keycloak && keycloak.login,
-        logout: keycloak && keycloak.logout,
+        logout: handleLogout,
         userPrivileges: userInfo?.tolkevarav?.privileges || [],
         institutionUserId: userInfo?.tolkevarav?.institutionUserId || '',
       }}
