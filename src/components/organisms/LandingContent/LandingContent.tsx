337484c21972f5d9a36ac14e3e68223748c5c32c
Maret Vaabel <maret@singleton.ee>
2023-12-14 16:09:21 +0200

    New-638-added machine translation text and button

--- src/components/organisms/LandingContent/LandingContent.tsx
+++ src/components/organisms/LandingContent/LandingContent.tsx
@@ -1,4 +1,7 @@
-import Button, { AppearanceTypes } from 'components/molecules/Button/Button'
+import Button, {
+  AppearanceTypes,
+  SizeTypes,
+} from 'components/molecules/Button/Button'
 import landingBackground from 'assets/landing_background.svg'
 import sponsorLogo from 'assets/sponsor-logo.jpg'
 import { useTranslation } from 'react-i18next'
@@ -16,32 +19,45 @@ const LandingContent: FC<LandingContentProps> = ({ className }) => {
   const { t } = useTranslation()
   const { login, isUserLoggedIn } = useAuth()
   return (
-    <div className={classNames(classes.container, className)}>
-      <div className={classes.whiteBackground} />
-      <img
-        src={landingBackground}
-        className={classes.background}
-        alt="Landing background"
-      />
-      <div className={classes.contentContainer}>
-        <h1>{t('landing.site_description')}</h1>
+    <>
+      <div className={classNames(classes.container, className)}>
+        <div className={classes.whiteBackground} />
+        <img
+          src={landingBackground}
+          className={classes.background}
+          alt="Landing background"
+        />
+        <div className={classes.contentContainer}>
+          <h1>{t('landing.site_description')}</h1>
 
-        <div className={classes.buttonsContainer}>
-          <Button
-            appearance={AppearanceTypes.Secondary}
-            children={t('button.check_tutorial')}
-            href={isUserLoggedIn ? '/manual' : undefined}
-            className={classes.bigButton}
-            hidden={!isUserLoggedIn}
-          />
+          <div className={classes.buttonsContainer}>
+            <Button
+              appearance={AppearanceTypes.Secondary}
+              children={t('button.check_tutorial')}
+              href={isUserLoggedIn ? '/manual' : undefined}
+              className={classes.bigButton}
+              hidden={!isUserLoggedIn}
+            />
+            <Button
+              appearance={AppearanceTypes.Primary}
+              children={
+                isUserLoggedIn ? t('button.add_new_project') : t('button.enter')
+              }
+              href={isUserLoggedIn ? '/projects/new-project' : undefined}
+              onClick={isUserLoggedIn ? undefined : login}
+              className={classes.button}
+            />
+          </div>
+          <h4 hidden={isUserLoggedIn} className={classes.text}>
+            {t('landing.machine_translation_text')}
+          </h4>
           <Button
             appearance={AppearanceTypes.Primary}
-            children={
-              isUserLoggedIn ? t('button.add_new_project') : t('button.enter')
-            }
-            href={isUserLoggedIn ? '/projects/new-project' : undefined}
-            onClick={isUserLoggedIn ? undefined : login}
-            className={classes.button}
+            children={t('button.click_here')}
+            href={'https://mtee.eki.ee/'}
+            target="_blank"
+            size={SizeTypes.S}
+            hidden={isUserLoggedIn}
           />
         </div>
       </div>
@@ -50,7 +66,7 @@ const LandingContent: FC<LandingContentProps> = ({ className }) => {
         className={classes.logo}
         alt="Euroopa Regionaalarengu Fond"
       />
-    </div>
+    </>
   )
 }
 
