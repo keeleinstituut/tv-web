830ed3202d13a4d3076a8d1772809e66b1282de7
Inga Sokman <inga@singleton.ee>
2024-03-27 15:41:16 +0200

    Wip-554 - Add focus to progressbar steps

--- src/components/atoms/ProgressBar/ProgressBar.tsx
+++ src/components/atoms/ProgressBar/ProgressBar.tsx
@@ -23,6 +23,16 @@ const ProgressBar: FC<ProgressBarProps> = ({
       {map(steps, (step, index) => {
         const { label } = step || {}
 
+        const handleKeyDown = (event: React.KeyboardEvent<HTMLLIElement>) => {
+          if (
+            event.type === 'click' ||
+            (event.type === 'keydown' &&
+              (event.key === 'Enter' || event.key === ' '))
+          ) {
+            setActiveStep(index + 1)
+          }
+        }
+
         return (
           <li
             key={index}
@@ -30,6 +40,8 @@ const ProgressBar: FC<ProgressBarProps> = ({
               [classes.currentStep]: activeStep === index + 1,
             })}
             onClick={() => setActiveStep(index + 1)}
+            tabIndex={0}
+            onKeyDown={handleKeyDown}
           >
             <span className={classes.stepNumber}>{index + 1}</span>
             <span className={classes.stepLabel}>{label}</span>
