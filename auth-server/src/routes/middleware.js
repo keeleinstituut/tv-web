f11d134fcbd2b24dc86c21f1815c7fb17a71e87c
dmytro <dmytro@fluidum.ee>
2025-12-18 13:56:49 +0200

    Add "SameSite" and "Secure" configuration for the `session-expires` cookie

--- auth-server/src/routes/middleware.js
+++ auth-server/src/routes/middleware.js
@@ -46,7 +46,14 @@ const autoRefreshAccessToken = () => async (req, res, next) => {
 
   if (!!accessToken) {
     const { exp } = jwtDecode(req.oidc.refreshToken) // Reread refresh token from session in case it was updated
-    res.cookie('session-expires', exp)
+
+    const isSecure = req.secure || req.protocol === 'https'
+    
+    res.cookie('session-expires', exp, {
+      sameSite: 'lax',
+      secure: isSecure,
+      httpOnly: false,
+    })
   } else {
     res.clearCookie('session-expires')
   }
