545104e02b9bc9385a04424948844bb8dd09b9f7
dmytro <dmytro@fluidum.ee>
2026-02-19 10:19:07 +0200

    hotfix: update a session state key for Keycloak 26 compatibility

--- auth-server/src/util.js
+++ auth-server/src/util.js
@@ -29,7 +29,7 @@ class AuditLogMessage {
     this.generalData.happened_at = new Date()
     this.generalData.actor_pic = parsedAccessToken.tolkevarav?.personalIdentificationCode
     this.generalData.actor_name = parsedAccessToken.tolkevarav?.forename + " " + parsedAccessToken.tolkevarav?.surname
-    this.generalData.actor_session = parsedAccessToken.session_state
+    this.generalData.actor_session = parsedAccessToken.sid
     this.generalData.actor_department_id = parsedAccessToken.tolkevarav?.department?.id
     this.generalData.actor_institution_id = parsedAccessToken.tolkevarav?.selectedInstitution?.id
     this.generalData.actor_institution_user_id = parsedAccessToken.tolkevarav?.institutionUserId
