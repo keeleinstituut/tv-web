6f48feb963a01dadec3524be4a9fb1df8fab0573
Markus <Markus>
2026-06-04 12:25:56 +0300

    remove obsolete api call

--- src/pages/TranslationMemoryPage/TranslationMemoryPage.tsx
+++ src/pages/TranslationMemoryPage/TranslationMemoryPage.tsx
@@ -21,7 +21,6 @@ const TranslationMemoryPage: FC = () => {
   const { translationMemory, isLoading, chunk_amount } = useFetchTranslationMemory({
     id: memoryId,
   })
-  const { tmChunkAmounts } = useFetchTmChunkAmounts({})
 
   const isTmOwnedByUserInstitution =
     selectedInstitution?.id === translationMemory?.institution_id
