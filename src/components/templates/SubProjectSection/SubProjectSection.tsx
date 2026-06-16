1f659d107f6a68928b37f0aa93e631ffec5966ae
dmytro <dmytro@fluidum.ee>
2026-06-16 15:31:08 +0300

    allow starting subproject workflows when assignment(s) has no candidates

--- src/components/templates/SubProjectSection/SubProjectSection.tsx
+++ src/components/templates/SubProjectSection/SubProjectSection.tsx
@@ -5,8 +5,8 @@ import {
   useSubProjectWorkflow,
 } from 'hooks/requests/useProjects'
 import Loader from 'components/atoms/Loader/Loader'
-import { includes, toLower, find, isEmpty } from 'lodash'
-import { ListSubProjectDetail, SubProjectFeatures } from 'types/projects'
+import { includes, toLower, find } from 'lodash'
+import { ListSubProjectDetail } from 'types/projects'
 import { useTranslation } from 'react-i18next'
 import ExpandableContentContainer from 'components/molecules/ExpandableContentContainer/ExpandableContentContainer'
 import classNames from 'classnames'
@@ -99,20 +99,12 @@ const SubProjectSection: FC<SubProjectProps> = ({
 
   const languageDirection = `${source_language_classifier_value?.value} > ${destination_language_classifier_value?.value}`
 
-  const hasAnyFeaturesWithoutCandidates = find(
-    assignments,
-    ({ candidates, job_definition }) =>
-      isEmpty(candidates) &&
-      job_definition?.job_key !== SubProjectFeatures.JobOverview
-  )
-
   const hasAnyAssignmentsWithoutDeadline = find(
     assignments,
     ({ deadline_at }) => !deadline_at
   )
 
-  const canStartWorkflow =
-    !hasAnyAssignmentsWithoutDeadline && !hasAnyFeaturesWithoutCandidates
+  const canStartWorkflow = !hasAnyAssignmentsWithoutDeadline
 
   const handleOpenContainer = useCallback(
     (isExpanded: boolean) => {
@@ -158,12 +150,6 @@ const SubProjectSection: FC<SubProjectProps> = ({
       wrapContent
       bottomComponent={
         <>
-          <Notification
-            content={t('warning.sub_project_tasks_missing_vendors')}
-            type={NotificationTypes.Warning}
-            className={classes.notificationStyle}
-            hidden={canStartWorkflow || isExpanded || workflow_started}
-          />
           <Notification
             content={t('warning.send_sub_project_to_vendor_warning')}
             hideIcon
