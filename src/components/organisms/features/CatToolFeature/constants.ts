b0624a9ed54d89e168f711605a07ab124b9cf394
Markus <Markus>
2026-09-17 08:27:28 +0300

    fix cat2 api url

--- src/components/organisms/features/CatToolFeature/constants.ts
+++ src/components/organisms/features/CatToolFeature/constants.ts
@@ -1,1 +1,11 @@
-export const CAT2_API_BASE_URL = "http://devbox.host:8000/cat2/api"
+const clean = (path: string) => path.replace(/^\//, '').replace('/$', '')
+
+const gateway = (path: string) => {
+  return (
+    import.meta.env.REACT_APP_GATEWAY_BASE?.replace(/\/$/, '') +
+    '/' +
+    clean(path)
+  )
+}
+
+export const CAT2_API_BASE_URL = gateway('/cat2/api')
